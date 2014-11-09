f30c56fb2833461040c4ecdcf82328ea8568f3e8
BobbyRuby <bobbyrubyii@gmail.com>
2014-11-08 19:41:53 -0500

    Backing up js file... working on conditions interface

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -107,6 +107,7 @@ jQuery(document).ready(function ($) {
     /***** Custom settings for pages ****/
     var htmlTagATTList = []; // holds our html attributes
     var shortcodeTagATTList = []; // holds our shortcode attributes
+    var conditionList = []; // holds our shortcode conditions
     var serializedMatchData;
 
         // prevent attribute_matching_form submit
@@ -121,7 +122,10 @@ jQuery(document).ready(function ($) {
         // prevent default posting of form
         event.preventDefault();
     });
-    // listen for the link to load our attributes for mapping
+
+    /**
+     * Begin listen for the link to load our attributes for mapping
+     */
     jQuery('#map_att_description a').click(function (e) {
         var html_att_options = '';
         var htmlTagATTs = [];
@@ -201,14 +205,157 @@ jQuery(document).ready(function ($) {
         }
         jQuery(form).html(html);
     });
+    /**
+     * End listen for the link to load our attributes for mapping
+     */
+    /**
+     * Begin conditions sections
+     */
+        // listen for radio options of the has_conditions question @todo - Make sure this errors out if there are no shortcode attrubutes
+    jQuery('[name="agsg_has_conditions"]').change(function (e) {
+        var val = jQuery(this).val();
+        if (val == 'Yes') {
+            conditionList.push('shortcode_condition_type');
+            var html = '<tr class="type_template"><th scope="row">Select an IF Statement Type</th><td><select id="shortcode_condition_type" name="agsg_shortcode_condition_type[]"><option value="select">Select IF statement type</option><option value="if">IF</option><option value="if-else">IF-ELSE</option><option value="if-elseif">IF-ELSEIF</option><option value="if-elseif-else">IF-ELSEIF-ELSE</option></select> ' +
+                '<label for="shortcode_condition_type"><span class="description"><br/>' +
+                '<span class="dashicons dashicons-welcome-write-blog"></span><span class="important">Important Note:</span> Use "IF" to do something when an attribute(s) evaluation is "TRUE".<span style="display: none;" class="dashicons dashicons-dismiss" title="Remove this condition and the data associated with it."></span><br/>' +
+                '<span class="dashicons dashicons-welcome-write-blog"></span><span class="important">Important Note:</span> Use "IF-ELSE" to do something when an attribute(s) evaluation is "TRUE" but something "else" if it evaluates to "FALSE".<br/>' +
+                '<span class="dashicons dashicons-welcome-write-blog"></span><span class="important">Important Note:</span> Use "IF-ELSEIF" to do something when one attribute(s) evaluation is "TRUE" or to do something when another attribute(s) evaluation is "TRUE"<br/>' +
+                '<span class="dashicons dashicons-welcome-write-blog"></span><span class="important">Important Note:</span> Use "IF-ELSEIF-ELSE" to do something when one attribute(s) evaluation is "TRUE" and if not, to do something when another attribute(s) evaluation is "TRUE", or to do something "else" if both attribute(s) evalutations is "FALSE".</span></label>' +
+                '</td></tr>';
+            jQuery(this).parent().parent().parent().after(html);
+            jQuery('[for="shortcode_condition_type"] .dashicons-dismiss').click(function (e) {
+                if (window.confirm("You will lose this condition and its' data. Continue?")) {
+                    jQuery(this).parent().parent().parent().parent().next().remove();
+                    jQuery(this).parent().parent().parent().parent().remove();
 
-    // listen for radio options of the has_atts question
+                    // cycle through list of condition ids
+                    for (var i = 0; i < conditionList.length; i++) {
+                        // check current id is the same as the -this-
+                        if (conditionList[i] === jQuery(this).parent().parent().prev().attr('id')) {
+                            conditionList[i] = 0; // if so set this index to 0 so it won't be used
+                        }
+                    }
+                }
+            });
+            jQuery('#shortcode_condition_type').change(function (e) {
+                var val = jQuery(this).find('option:selected').val();
+                var html = '';
+                if (jQuery(this).attr('id') == 'shortcode_condition_type') {
+                    var condition_id_num = jQuery(this).attr('id').replace('shortcode_condition_type', '');
+                    condition_id_num = 1;
+                } else {
+                    var condition_id_num = jQuery(this).attr('id').replace('shortcode_condition_type_', '');
+                }
+                if (val == 'select') {
+                    jQuery(this).parent().parent().parent().find('.for_condition_number_' + condition_id_num).remove();
+                }
+                else if (val == 'if') {
+                    // attribute to evaluate
+                    html += '<tr class="for_condition_number_' + condition_id_num + '"><th scope="row">Evaluate Attribute</th><td><select class="shortcode_condition_attribute" name="agsg_shortcode_condition_' + condition_id_num + '_attribute">';
+                    html += getShortcodeAttributeOptionsList();
+                    html += '</select><label for="shortcode_condition_attribute"><span class="description">This is the shortcode attribute you want to check the value of.</span></label>' +
+                        '<div id="reset_for_attribute_condition_number_' + condition_id_num + '" class="reset_condition_container"><span class="reset_condition">Reset these options</span><span class="dashicons dashicons-randomize"></span></div></td></tr>';
+
+                    // operator type == / <= / >= / < / >
+                    html += '<tr class="for_condition_number_' + condition_id_num + '"><th scope="row">Operator</th><td><select class="shortcode_condition_operator" name="agsg_shortcode_condition_' + condition_id_num + '_operator">';
+                    html += '<option value="==">equal to</option>' +
+                        '<option value="!=">equal to</option>' +
+                        '<option value="<=">less than or equal to</option>' +
+                        '<option value="<">less than</option>' +
+                        '<option value=">=">greater than or equal to</option>' +
+                        '<option value=">">greater than</option>' +
+                        '</select><label for="shortcode_condition_operator"><span class="description">This is the operator to use in evaluating the shortcode attribute you want to check against the value you input in the text field below.</span></label>';
+                    // value to check against
+                    html += '<tr class="for_condition_number_' + condition_id_num + '"><th scope="row">Value</th><td><input class="shortcode_condition_value" type="text" value="" placeholder="" name="agsg_shortcode_condition_' + condition_id_num + '_value">' +
+                        '<label for="agsg_shortcode_condition_' + condition_id_num + '_value"><span class="description">This is the value the value to check the shortcode attribute against using the operator above.</span></label>' +
+                        '</td></tr>';
+                    // action to do if true
+                }
+                else if (val == 'if-else') {
+                    // attribute to evaluate
+                    // operator type == / <= / >= / < / >
+                    // value to check against
+                    // action to do if true
+                    // action to do if false
+                }
+                else if (val == 'if-elseif') {
+                    // 1st attribute to evaluate
+                    // operator type == / <= / >= / < / >
+                    // value to check against
+                    // action to do if true
+
+                    // 2nd attribute to evaluate
+                    // operator type == / <= / >= / < / >
+                    // value to check against
+                    // action to do if true
+                }
+                else if (val == 'if-elseif-else') {
+                    // 1st attribute to evaluate
+                    // operator type == / <= / >= / < / >
+                    // value to check against
+                    // action to do if true
+
+                    // 2nd attribute to evaluate
+                    // operator type == / <= / >= / < / >
+                    // value to check against
+                    // action to do if true
+
+                    // action to do if both are false
+                }
+                jQuery(this).parent().parent().after(html);
+                jQuery('#reset_for_attribute_condition_number_' + condition_id_num).click(function (e) {
+                    jQuery('[name="agsg_shortcode_condition_' + condition_id_num + '_attribute"]').html(getShortcodeAttributeOptionsList('reset'));
+                });
+            });
+            jQuery('#add_shortcode_condition').show();
+        } else {
+            if (window.confirm("You will lose all conditions. Continue?")) {
+                jQuery('[name="agsg_shortcode_condition_type[]"]').parent().parent().remove();
+                jQuery('.shortcode_condition_attribute').parent().parent().remove();
+                jQuery('#add_shortcode_condition').hide();
+            }
+            else { // canceled
+                jQuery('#has_conditions_Yes').attr('checked', 'checked');
+            }
+        }
+    });
+    // listen for add button
+    var shortcode_condition_type_count = jQuery('[name="agsg_shortcode_condition_type[]"]').length;
+    jQuery('#add_shortcode_condition').click(function (e) {
+        // count the types
+        shortcode_condition_type_count++;
+        var count = shortcode_condition_type_count + 1;
+
+        var type_clone = jQuery('.type_template').clone(true, true).removeClass('type_template').removeClass('form-invalid');
+        jQuery("th", type_clone).text('Select an IF Statement Type ' + count);
+        jQuery("select", type_clone).attr('id', jQuery("select", type_clone).attr('id') + '_' + count);
+        jQuery("label", type_clone).attr('for', jQuery("label", type_clone).attr('for') + '_' + count);
+        jQuery(".error", type_clone).remove();
+        jQuery(".dashicons-dismiss", type_clone).show();
+
+        conditionList.push('shortcode_condition_type_' + count);
+
+        count--;
+        if (!count == 1) {
+            jQuery('#shortcode_condition_type' + '_' + count).parent().parent().parent().append(type_clone);
+        } else {
+            jQuery('#shortcode_condition_type').parent().parent().parent().append(type_clone);
+        }
+    });
+    /**
+     * End conditions section
+     */
+    /**
+     * Begin html tag section
+     */
+        // listen for radio options of the has_html_tag_atts question
     jQuery('[name="agsg_has_html_tag_atts"]').change(function (e) {
         var val = jQuery(this).val();
         if (val == 'Yes') {
             htmlTagATTList.push('html_tag_att_name');
             var html = '<tr class="html_tag_template_name"><th scope="row">HTML TAG Attribute 1 Name</th><td><input type="text" value="" placeholder="" name="agsg_html_tag_att_name[]" id="html_tag_att_name">' +
-                '<label for="html_tag_att_name"><span class="description">This is the name for the HTML TAG attribute.<span style="display: none;" class="dashicons dashicons-dismiss" title="Remove this attribute and the static vaule associated with it."></span></span></label>' +
+                '<label for="html_tag_att_name"><span class="description">This is the name for the HTML TAG attribute.<span style="display: none;" class="dashicons dashicons-dismiss" title="Remove this attribute and the static value associated with it."></span></span></label>' +
                 '</td></tr>';
             html += '<tr class="html_tag_template_value"><th scope="row">HTML TAG Attribute 1 Set Value</th><td><input type="text" value="" placeholder="" name="agsg_html_tag_default[]" id="html_tag_default">' +
                 '<label for="html_tag_default"><span class="description">The SET value for the attribute.<br/>  ' +
@@ -249,11 +396,12 @@ jQuery(document).ready(function ($) {
         }
     });
     // listen for add attribute button
+    var html_tag_att_count = jQuery('[name="agsg_html_tag_att_name[]"]').length;
     jQuery('#add_html_tag_att').click(function (e) {
         // count the names ( defaults will be the same)
-        var count = jQuery('[name="agsg_html_tag_att_name[]"]').length;
-        console.log(count);
-        count++;
+        html_tag_att_count++;
+        var count = html_tag_att_count + 1;
+
         var name_clone = jQuery('.html_tag_template_name').clone(true, true).removeClass('html_tag_template_name').removeClass('form-invalid');
         jQuery("th", name_clone).text('HTML TAG Attribute ' + count + ' Name');
         jQuery("input", name_clone).attr('id', jQuery("input", name_clone).attr('id') + '_' + count);
@@ -275,7 +423,13 @@ jQuery(document).ready(function ($) {
             jQuery('#html_tag_default').parent().parent().after(value_clone).after(name_clone);
         }
     });
-    // listen for radio options of the has_atts question
+    /**
+     * End html tag section
+     */
+    /**
+     * Begin atts section
+     */
+        // listen for radio options of the has_atts question
     jQuery('[name="agsg_has_atts"]').change(function (e) {
         var val = jQuery(this).val();
         if (val == 'Yes') {
@@ -289,7 +443,7 @@ jQuery(document).ready(function ($) {
             jQuery(this).parent().parent().parent().after(html);
             jQuery('[for="att_name"] .dashicons-dismiss').click(function (e) {
 //                if(jQuery('#has_atts_Yes').is(':checked')){
-                if (window.confirm("You will lose this attribute and must reset attribute mapping..")) {
+                if (window.confirm("You will lose this attribute, you must reset attribute mapping, and if you were using this for a condition you need to delete is as well.  Continue?")) {
                     serializedMatchData = 0;
                     jQuery(this).parent().parent().parent().parent().next().remove();
                     jQuery(this).parent().parent().parent().parent().remove();
@@ -321,17 +475,20 @@ jQuery(document).ready(function ($) {
         }
     });
     // listen for add attribute button
+    var att_name_count = jQuery('[name="agsg_att_name[]"]').length;
     jQuery('#add_shortcode_att').click(function (e) {
         // count the names ( defaults will be the same)
-        var count = jQuery('[name="agsg_att_name[]"]').length;
-        console.log(count);
-        count++;
+        att_name_count++;
+        var count = att_name_count + 1;
+
         var name_clone = jQuery('.template_name').clone(true, true).removeClass('template_name').removeClass('form-invalid');
         jQuery("th", name_clone).text('Attribute ' + count + ' Name');
         jQuery("input", name_clone).attr('id', jQuery("input", name_clone).attr('id') + '_' + count);
         jQuery("label", name_clone).attr('for', jQuery("label", name_clone).attr('for') + '_' + count);
         jQuery(".error", name_clone).remove();
+
         shortcodeTagATTList.push('att_name_' + count);
+
         jQuery(".dashicons-dismiss", name_clone).show();
         var value_clone = jQuery('.template_value').clone(true, true).removeClass('template_value');
         jQuery("th", value_clone).text('Attribute ' + count + ' Value');
@@ -345,7 +502,13 @@ jQuery(document).ready(function ($) {
             jQuery('#default').parent().parent().after(value_clone).after(name_clone);
         }
     });
-    // listen for shortcode tag to keep input valid
+    /**
+     * End atts section
+     */
+    /**
+     * Begin field .change modifications to force valid input
+     */
+        // listen for shortcode tag to keep input valid
     jQuery('[name="agsg_shortcode_tag_name"]').change(function (e) {
         var val = jQuery(this).val();
         val = val.replace(/[\[\]]/g, '', 'gi').trim().replace(' ', '_', 'gi');
@@ -358,12 +521,15 @@ jQuery(document).ready(function ($) {
         jQuery(this).val(val);
     });
     /**
-     * Shortcode Generator Forms
+     * End field .change modifications to force valid input
+     */
+    /**
+     * Begin Shortcode Generator Forms Post Code
      */
     var dir = jQuery('[name="agsg_install_url"]').val(); // dir url of install from hidden meta
     var page = dir + 'class-agsgPlugin.php';
     /**
-     * eagsg page form
+     * Begin eagsg page form
      */
     var request = '';
     jQuery("#agsg_eagsg_form").submit(function (event) {
@@ -445,4 +611,27 @@ jQuery(document).ready(function ($) {
         // prevent default posting of form
         event.preventDefault();
     });
+    /**
+     * End eagsg page form
+     */
+    /**
+     * End Shortcode Generator Forms Post Code
+     */
 });
+/**
+ * Build a list of shortcode attribute options in HTML
+ * @returns {string}
+ */
+function getShortcodeAttributeOptionsList(reset) {
+    if (reset === 'reset') {
+        var html = '<option value="select" selected="selected">Reset - Select Attribute</option>';
+    } else {
+        var html = '<option value="select" selected="selected">Select Attribute</option>';
+    }
+    // build a list of options from the attributes
+    jQuery('[name="agsg_att_name[]"]').each(function (e) {
+        var oVal = jQuery(this).val();
+        html += '<option value="' + oVal + '">' + oVal + '</option>';
+    });
+    return html;
+}
