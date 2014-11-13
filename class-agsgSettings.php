79e31aeb50376ab2f838d0682425d7f97fdf4916
BobbyRuby <bobbyrubyii@gmail.com>
2014-11-13 13:15:22 -0500

    Minor description change.

--- class-agsgSettings.php
+++ class-agsgSettings.php
@@ -232,7 +232,7 @@ class agsgSettings
                     'description' => __('<p>With this generator you can generate shortcodes that are from the most complicated to the most basic.  It was built to allow those without programming knowledge to create shortcodes in which they could have never dreamed.</p>
                     <p>These shortcodes include those that embed videos or complete custom layouts and much much more.<br/>
                      <strong>It does this by providing the following features for generating code:</strong>
-                      <ol class="description">
+                      <ul class="description">
                         <li>Add HTML attriutes to wrapper elements for enclosing shortcodes.</li>
                         <li>Add attriutes to shortcodes which you can use for lots of different implementations.</li>
                         <li>Allow the mapping of shortcode attributes to HTML attributes.</li>
@@ -240,7 +240,7 @@ class agsgSettings
                         <li>A custom implementation of <a href="http://tinymce.com/" target="_blank" title="TinyMCE">TinyMCE</a> which allows the creation of tables, divs, images, video embeds and more to add customized content that is conditionally displayed while providing a custom "attribute reference" syntax to allow you to place attributed values within the content you want conditionally displayed.</li>
                         <li>Add conditonal statements to shortcodes so you can display additional content on a per use basis when using enclosed shortcodes or when using self closed shortcodes, replace the shortcode with content conditionally per use. The content is displayed based on the value of an attribute you set up within a condition using the AGSG. The content also may contain references to other attibutes via the "attribute reference" syntax --> "&lt;&lt;i_am_an_attribute&gt;&gt;".</li>
                         <li>Make enclosed shortcodes process other shortcodes.</li>
-                       </ol>
+                       </ul>
                     </p>
                     <p>A pro version is in the works that implements even more functionality!  Feartures being considered are making it easy to create a recent posts shortcode, recent facebook updates, twitter updates, and more stuff.<br>
                      If this plugin has helped you out then buy me a beer at the bar! <a target="_blank" href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=MbR34ArD7X0ecLR9elSOxj9ZNg5JSTHtJsdhcajRVGkmR48AW6EMhoU4uju&dispatch=5885d80a13c0db1f8e263663d3faee8d66f31424b43e9a70645c907a6cbd8fb4" title="Buy me a Beer!">Donate to support the future development of this FREE plugin!</a></p>', 'plugin_textdomain'),
